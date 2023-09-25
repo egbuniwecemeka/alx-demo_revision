0cec044da1709a0244a2b404bc05168047deb20c
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-09-25 23:00:11 +0100

    list insertion

---
+++ 0x08-LinkedList/README.md
@@ -0,0 +1,8 @@
+Arrays usually consist data types of similar types while linked lists can contain data types of different types
+They consist of a node a link. The node consists of data to be stored and a pointer to the next node.
+In arrays, size is limited to definition, but in linked list size is not limited/defined size
+3 types namely
+
+- Singly linked list - points to next node
+- Double linked list - points to previous and next node
+- Circular linked list - last node points to first node. It could be single or double linked
---
+++ 0x08-LinkedList/insertion.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Basic Operations in the Linked Lists
+ * Insertion - 3 methods
+ * Deletion
+ * Updating
+ * Displaying
+ * Searching
+ */
+
+/** Insertion flowchart
+ * For a list to be inserted between to nodes A & B, Firstly, create a node of same structure with the location it is to be inserted identified (C). 
+ * i.e C->B && A->C
+ */
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+struct node *current = NULL;
+
+void printList()
+{
+    struct node *p = head;
+    printf("\n[")
+    while (p != NULL)
+    {
+        printf(" %d ", p->data);
+        p = p->next 
+    }
+    printf("]\n");
+}
+
+void insertatfirst(int data)
+{
+    //Create link
+    struct node *link = (struct node *)malloc(sizeof(struct node));
+    link->data = data;
+
+    //Point to old first node
+    link->next = head;
+
+    //Point to new node
+    head = link;
+}
+
+int main()
+{
+    insertatfirst(10);
+    insertatfirst(30);
+    printf("Linked list: ");
+
+    //Print List
+    printList();
+}
