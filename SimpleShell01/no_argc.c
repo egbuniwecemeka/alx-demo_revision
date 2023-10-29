4a3fa7606236cc6c60a6ec86b7ecb40e8dc3f0a9
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-10-29 23:37:06 +0100

    updates

---
+++ SimpleShell01/no_argc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * 
+ */
+
+int main(int ac, char **av)
+{
+    int i;
+
+    while (*av != NULL)
+    {
+        printf("argv[%d] = %s\n", *av);
+        av[i];
+        i++;
+    }
+    
+}
