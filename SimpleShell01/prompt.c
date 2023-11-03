55a83323435309e2dc77e4a2245b486a29905994
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-11-03 14:38:57 -0700

    prompt.c added

--- SimpleShell01/prompt.c
+++ SimpleShell01/prompt.c
@@ -7,12 +7,20 @@ int main(void)
     char *prompt = "$ ";
     char *lineptr = NULL;
     size_t n = 0;
+    ssize_t readByte;
 
     write(STDOUT_FILENO, prompt, 2);
 
-    getline(&lineptr, &n, stdin);
+    while ((readByte = getline(&lineptr, &n, stdin)) != -1)
+	{		if (readByte > 1)
+		{
+			write(STDOUT_FILENO, lineptr, readByte);
+		}
+		write(STDOUT_FILENO, prompt, 2);
+	}
 
-    printf("%s\n", lineptr);
+
+    free(lineptr);
 
     return (0);
 }
