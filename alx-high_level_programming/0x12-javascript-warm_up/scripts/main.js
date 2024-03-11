3df2e00ab0fa5a6d6b460c0197d92e2b2c8dab0c
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-11 18:11:07 +0000

    eventlistener

--- alx-high_level_programming/0x12-javascript-warm_up/scripts/main.js
+++ alx-high_level_programming/0x12-javascript-warm_up/scripts/main.js
@@ -1,2 +1,8 @@
 const myHeader = document.querySelector("h1");
 myHeader.textContent = "Hello World";
+
+const clickEvent = document.querySelector("html");
+clickEvent.addEventListener("click", function(){
+	alert("This is a click event listener")
+});
+
