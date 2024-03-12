54d7dc8ee554a1903f5f1f413ed0a64a66366489
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-12 10:07:21 +0000

    image update

--- alx-high_level_programming/0x12-javascript-warm_up/scripts/main.js
+++ alx-high_level_programming/0x12-javascript-warm_up/scripts/main.js
@@ -1,11 +1,3 @@
-const myHeader = document.querySelector("h1");
-myHeader.textContent = "Hello World";
-
-const clickEvent = document.querySelector("html");
-clickEvent.addEventListener("click", () => {
-	alert("This is a click event listener")
-});
-
 const myImg  = document.querySelector("img");
 
 myImg.onclick = () => {
