b4f5ff8ff920a2ddc3fe6a1d87170856abc62368
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-06-24 13:24:27 -0700

    JavaScript

---
+++ JavaScript/0x12-javascript-warm_up/study/defaultParameter.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Default parameters can be used in JavaScript by '='
+
+function hello(name='Emma'){
+	console.log(`Hello ${name}`);
+}
+
+hello('Egbuniwe');
+hello();
---
+++ JavaScript/0x12-javascript-warm_up/study/textReplace.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const text = "I am a Python programmer";
+
+const textReplace = text.replace("Python", "JavaScript");
+
+console.log(textReplace);
