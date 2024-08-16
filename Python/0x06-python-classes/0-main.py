5b05fdae666802049d4e3a50402794b0b7ca4c9b
egbuniwecemeka <egbuniwecemeka.com>
2024-08-16 09:18:44 +0100

    new print test added

---
+++ Python/0x06-python-classes/0-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+Square = __import__('0-square').Square
+
+# object/instance of the class Square
+mysquare = Square()
+print(mysquare)
+print(type(mysquare))
+print(mysquare.__dict__)
