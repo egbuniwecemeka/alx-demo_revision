04abbfdd37159ad17d6979aac72d1b21410021b1
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-16 16:20:21 -0700

    self att added

--- Python/0x06-python-classes/oop_method.py
+++ Python/0x06-python-classes/oop_method.py
@@ -2,7 +2,7 @@
 """ A python script that displays a greeting using a method"""
 
 class Greet:
-    def say_hi():
+    def say_hi(self):
         print('Hello Good Morning!')
 
 if __name__ == "__main__":
