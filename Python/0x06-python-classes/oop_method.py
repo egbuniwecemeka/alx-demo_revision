ad3de2188c434c83d3bc856689dc15190de6f7f0
egbuniwecemeka <egbuniwecemeka.com>
2024-08-16 09:17:37 +0100

    methods

---
+++ Python/0x06-python-classes/oop_method.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A python script that displays a greeting using a method"""
+
+class Greet:
+    def say_hi():
+        print('Hello Good Morning!')
+
+if __name__ == "__main__":
+    morning = Greet()
+    morning.say_hi()
