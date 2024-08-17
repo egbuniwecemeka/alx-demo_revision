a56a8681ac6eb87b2efbe207affe9ef5bc7e3761
egbuniwecemeka <egbuniwecemeka.com>
2024-08-17 02:35:18 +0100

    classes and objects variables and namespaces

--- Python/0x06-python-classes/oop_objvar.py
+++ Python/0x06-python-classes/oop_objvar.py
@@ -12,7 +12,9 @@ class Robot:
         self.name = name
         print(f"Initialize to {self.name}")
 
+        # When robot is created, it is added to the total
         Robot.total_robots += 1
+
 
     def say_hi(self):
         """ Introduce robot """
@@ -20,6 +22,8 @@ class Robot:
     
     def kill(self):
         """Kill robot """
+
+        print(f"Robot {self.name} is been terminated")
 
         Robot.total_robots -= 1
         
@@ -31,7 +35,7 @@ class Robot:
     @classmethod
     def robot_count(cls):
         """ Retrieves the total number of robots"""
-        print(f"Total robots => {cls.robot_count}")
+        print(f"Total robots => {cls.total_robots}")
 
 r1 = Robot('Abc')
 r1.say_hi()
