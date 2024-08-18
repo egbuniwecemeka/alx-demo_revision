d9e51b8c650137adcef92237d9415f0029f85b48
egbuniwecemeka <egbuniwecemeka.com>
2024-08-18 07:51:57 +0100

    object initialization

--- Python/0x06-python-classes/oop_init.py
+++ Python/0x06-python-classes/oop_init.py
@@ -3,15 +3,21 @@
 
 class Staff:
     """ Staff class """
-    def __init__(self, name):
+    def __init__(self, name=None):
         """ Initialize values to objects"""
         self.name = name
     
     def greet_staff(self):
-        print(f"The new staff name is {self.name}")
+        if self.name:
+            print(f"The new staff name is {self.name}")
+        else:
+            print(f"The new staff is yet to be employed")
     
 
 if __name__ == "__main__":
     """ Executes when run only as main script """
-    staff = Staff("Basil")
+    staff = Staff()
     staff.greet_staff()
+
+    staff1 = Staff("Basil")
+    staff1.greet_staff()
