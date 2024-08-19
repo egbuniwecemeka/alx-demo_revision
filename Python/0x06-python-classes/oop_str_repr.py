ec8e78d82a6cbe3944836ac830139daaff83800b
egbuniwecemeka <egbuniwecemeka.com>
2024-08-19 16:47:19 +0100

    str and repr methods

--- Python/0x06-python-classes/oop_str_repr.py
+++ Python/0x06-python-classes/oop_str_repr.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
 """ Understanding usage of str and repr magic methods """
 
+""" import datetime
+
+today = datetime.datetime()
+rep_r = repr(today)
+# Note that str cannot be parsed into an object using eval
+r = eval(rep_r)
+print(r)
+print(type(r))
+
 language = ["Python", "C", "JavaScript", "MySQL"]
 car = {"benz":2, "bmw": 1}
 x = 9
-
+# The below formats the string representation of their data types.
 print(str(language))
 print(repr(language))
 print(str(car))
 print(repr(car))
 print(str(x))
-print(repr(x))
+print(repr(x)) """
 
 class Player:
-    def __repr__(self) -> int:
-        return 42
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
 
-p = Player()
-print(repr(p))
-print(str(p)) # defaults to repr if str is not specified in the class
+    def __repr__(self):
+        return "Robot('" + self.name + "', " + self.year +")"
+    
+if __name__ == "__main__":
+        p = Player("Emeka", 59)
+        p_str = str(p)
+        print(p_str)
+        print("Type of string:", type(p_str))
+        new = eval(p_str)
+        print("Type of new:", type(new))
