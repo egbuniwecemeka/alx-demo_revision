b03baef9702875328a742112b84b17bcb6dac8d6
egbuniwecemeka <egbuniwecemeka.com>
2024-08-19 13:12:39 +0100

    str and repr methods

---
+++ Python/0x06-python-classes/oop_str_repr.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Understanding usage of str and repr magic methods """
+
+language = ["Python", "C", "JavaScript", "MySQL"]
+car = {"benz":2, "bmw": 1}
+x = 9
+
+print(str(language))
+print(repr(language))
+print(str(car))
+print(repr(car))
+print(str(x))
+print(repr(x))
+
+class Player:
+    def __repr__(self) -> str:
+        return 42
+
+p = Player()
+print(repr(p))
+print(str(p)) # defaults to repr if str is not specified in the class
