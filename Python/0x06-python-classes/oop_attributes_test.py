332b81ed22710cb2c76b9b9014a7def984fc0532
egbuniwecemeka <egbuniwecemeka.com>
2024-08-21 03:01:57 +0100

    Attribute types

---
+++ Python/0x06-python-classes/oop_attributes_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A simple class testing class attributes"""
+
+class Attribute:
+    def __init__(self):
+        self.public = "I am public"
+        self._protected = "I am protected"
+        self.__private = "I am private"
